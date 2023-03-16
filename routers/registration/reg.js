a8bc9a227286ad7652d64c0069e246374c5495ac
kddco <52121443+kddco@users.noreply.github.com>
2023-03-16 16:08:59 +0800

    檢查session和當前請求值一致

--- routers/registration/reg.js
+++ routers/registration/reg.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-
+// router.use(sessionMiddleware)
 // router.get('/preauthenticate' , (req , res)=>{
 //     // router code here
 //     res.send('/preauthenticate');
@@ -16,7 +16,7 @@ router.post('/preregister', (req, res) => {
     
     const { rp,user } = req.body;
     const{reqid,type,app} = rp;
-    const {id,name,displayName} = user;
+    const {ID,name,displayName} = user;
     
     var challenge = require('../../challenge_fun.js');
     challenge_code = challenge.get_128bits();
@@ -24,7 +24,7 @@ router.post('/preregister', (req, res) => {
     
     //驗證所有參數是否存在
     if((!rp || typeof rp.reqid === 'undefined' || typeof rp.type === 'undefined' || typeof rp.app === 'undefined' ||
-    typeof user.id === 'undefined' || typeof user.name === 'undefined' || typeof user.displayName === 'undefined')){
+    typeof user.ID === 'undefined' || typeof user.name === 'undefined' || typeof user.displayName === 'undefined')){
       res.status(400).send('Missing required parameters');
       return;
     }
@@ -32,10 +32,10 @@ router.post('/preregister', (req, res) => {
       req.session.reqid = reqid;
       req.session.type = type;
       req.session.app=app;
-      req.session.id=id;
+      req.session.ID=ID;
       req.session.name=name;
       req.session.displayName=displayName;
-      console.log("前註冊 req.session.reqid：",req.session.reqid);
+      // console.log("前註冊 req.session.reqid：",req.session.reqid);
       // console.log("req.sessionID：",req.sessionID);
 
 
@@ -49,7 +49,7 @@ router.post('/preregister', (req, res) => {
         "app": app
       },
       "user": {
-        "id": id,
+        "ID": ID,
         "name": name,
         "displayName": displayName
       },
@@ -72,50 +72,53 @@ router.post('/preregister', (req, res) => {
 router.post('/register' , (req , res,)=>{
     // router code here
     // res.send('/register');
-    console.log(req.session);
+
     const { rp,user } = req.body;
     const{reqid,type,app} = rp;
-    const {id,name,displayName} = user;
+    const {ID,name,displayName} = user;
     
     var challenge = require('../../challenge_fun.js');
 
-    if(!req.session){
+    //查看session是否有被設定
+    if (typeof req.session === 'undefined') {
       res.status(400).send('please preregister first，session不存在');
       return;
 
     }
+    //檢查session中的值是否為空(無先行呼叫/preregister)
     if (!req.session.reqid || !req.session.type || !req.session.app ||
-      !req.session.id || !req.session.name || !req.session.displayName) {
+      !req.session.ID || !req.session.name || !req.session.displayName) {
       res.status(400).send('Please call /preregister first');
       return;
     }
 
-    //檢查JSON格式中值是否有空
+    //檢查當前請求的JSON格式是否正確
     if((!rp || typeof rp.reqid === 'undefined' || typeof rp.type === 'undefined' || typeof rp.app === 'undefined' ||
-    typeof user.id === 'undefined' || typeof user.name === 'undefined' || typeof user.displayName === 'undefined')){
+    typeof user.ID === 'undefined' || typeof user.name === 'undefined' || typeof user.displayName === 'undefined')){
       res.status(400).send('please preregister first,參數錯誤');
       
       return;
     }
-    console.log("註冊 req.session.displayName",req.session.displayName);
-    // console.log(req.session.rp);
-    // if (req.session && req.session.reqid && req.session.type && req.session.app
-    //   && req.session.id && req.session.name && req.session.displayName) {
-    if (req.session) {
+    if (!(req.session.reqid === req.body.rp.reqid
+    && req.session.type === req.body.rp.type
+    && req.session.app === req.body.rp.app
+    && req.session.ID === req.body.user.ID
+    && req.session.name === req.body.user.name
+    && req.session.displayName === req.body.user.displayName)) {
+    // if (req.session.ID === req.body.user.ID){
+      console.log("屬性值不一致");
+      res.status(400).send('前註冊和註冊的屬性值不一致');
+    }
 
+    if (req.session) {
         // 如果會話已經存在，則返回歡迎信息
-        // console.log(req.session);
-        
+        // console.log(req.session);    
         res.status(200).send(`Welcome back ${req.session.displayName}!`);
         return;
-
-
       }
-      else{
-        
+      else{      
         res.status(400).send('session不存在');
         return;
-        
       }
 
 
