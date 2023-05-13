2153e526e963da0464ca0e3cc09925b5d7c7652c
kddco <52121443+kddco@users.noreply.github.com>
2023-05-14 01:07:29 +0800

    分類函式
    增加可讀性

--- ECDH_server.js
+++ ECDH_server.js
@@ -1,58 +1,61 @@
 const crypto = require('crypto');
 const fs = require('fs');
-// 生成ECDH對象
 
-const serverPublicKeyHex = fs.readFileSync('ServerPublicKey.txt', 'utf8');
-const serverPrivateKeyHex = fs.readFileSync('ServerPrivateKey.txt', 'utf8');
-
-
-// 將hex格式的鑰匙轉換為Buffer
-const serverPublicKey = Buffer.from(serverPublicKeyHex, 'hex');
-const serverPrivateKey = Buffer.from(serverPrivateKeyHex, 'hex');
-
-// 創建ECDH對象
-const server_keypair = crypto.createECDH('secp256k1');
-
-// 設置公鑰和私鑰
-server_keypair.setPublicKey(serverPublicKey);
-server_keypair.setPrivateKey(serverPrivateKey);
-
-
-
-
-
-async function getClientPublicKeyHex(displayName,name){
+async function getClientPublicKeyHex(displayName, name){
     const db_fun = require('./db_fun.js');
-    const publicKeyHex = await db_fun.connectionDB_login_find(displayName,name);
+    const publicKeyHex = await db_fun.connectionDB_login_find(displayName, name);
     return publicKeyHex;
+}
+
+function getServerKeyPair() {
+    const serverPublicKeyHex = fs.readFileSync('ServerPublicKey.txt', 'utf8');
+    const serverPrivateKeyHex = fs.readFileSync('ServerPrivateKey.txt', 'utf8');
+    const serverPublicKey = Buffer.from(serverPublicKeyHex, 'hex');
+    const serverPrivateKey = Buffer.from(serverPrivateKeyHex, 'hex');
+
+    const server_keypair = crypto.createECDH('secp256k1');
+    server_keypair.setPublicKey(serverPublicKey);
+    server_keypair.setPrivateKey(serverPrivateKey);
+
+    return server_keypair;
+}
+
+function computeSharedSecret(server_keypair, ClientPublicKey) {
+    return server_keypair.computeSecret(ClientPublicKey);
+}
+
+function encryptMessage(sharedSecret, message) {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv('aes-256-gcm', sharedSecret.slice(0, 32), iv);
+    let encrypted = cipher.update(message, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    const tag = cipher.getAuthTag();
+
+    return { encrypted, tag, iv };
+}
+
+function decryptMessage(sharedSecret, encrypted, tag, iv) {
+    const decipher = crypto.createDecipheriv('aes-256-gcm', sharedSecret.slice(0, 32), iv);
+    decipher.setAuthTag(tag);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+
+    return decrypted;
 }
 
 async function main(){
     const ClientPublicKeyHex = await getClientPublicKeyHex("MydisplayName", "Myname");
-    // await new Promise(resolve => setTimeout(resolve, 3000)); // 程式暫停 3 秒
-    console.log("ClientPublicKeyHex",ClientPublicKeyHex);
     const ClientPublicKey = Buffer.from(ClientPublicKeyHex, 'hex');
-    
 
+    const server_keypair = getServerKeyPair();
+    const sharedSecret = computeSharedSecret(server_keypair, ClientPublicKey);
 
-    const serverPublicKeyHex = fs.readFileSync('ServerPublicKey.txt', 'utf8');
-    const serverPrivateKeyHex = fs.readFileSync('ServerPrivateKey.txt', 'utf8');
+    const { encrypted, tag, iv } = encryptMessage(sharedSecret, 'Hello, world!');
+    console.log('Encrypted message: ', encrypted);
+    console.log('Authentication tag: ', tag.toString('hex'));
 
-
-    // 將hex格式的鑰匙轉換為Buffer
-    const serverPublicKey = Buffer.from(serverPublicKeyHex, 'hex');
-    const serverPrivateKey = Buffer.from(serverPrivateKeyHex, 'hex');
-
-    // 創建ECDH對象
-    const server_keypair = crypto.createECDH('secp256k1');
-
-    // 設置公鑰和私鑰
-    server_keypair.setPublicKey(serverPublicKey);
-    server_keypair.setPrivateKey(serverPrivateKey);
-
-    //// 使用對方的公鑰和你的私鑰生成共享的會話鑰匙
-    const sharedSecret = server_keypair.computeSecret(ClientPublicKey);
-    console.log('Shared secret: ', sharedSecret.toString('hex'));
-
+    const decrypted = decryptMessage(sharedSecret, encrypted, tag, iv);
+    console.log('Decrypted message: ', decrypted);
 }
+
 main();
